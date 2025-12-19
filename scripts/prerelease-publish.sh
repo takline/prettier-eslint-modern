e9fad2714e4aa5f02dcbd7e7967712953ceb2587
takline <tylerkline@gmail.com>
2025-12-19 11:57:03 -0500

    fixing publish workflow

--- scripts/prerelease-publish.sh
+++ scripts/prerelease-publish.sh
@@ -1,4 +1,2 @@
-npm version "$1" --no-git-tag-version
 bun run package
 bun run vsce publish --pre-release -p "$VSCE_PAT"
-bun run ovsx publish --pre-release -p "$OVSX_PAT"
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,2 @@
-npm version "$1" --no-git-tag-version
 bun run package
 bun run vsce publish -p "$VSCE_PAT"
-bun run ovsx publish -p "$OVSX_PAT"
